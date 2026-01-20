544accea54dc50e9e24eb07f6cf87722a5501d7f
hwangjihun <e0959872@u.nus.edu>
2026-01-21 02:31:56 +0800

    Renamed chatbot, added a basic greet/exit

---
+++ src/main/java/Hunnie.java
@@ -0,0 +1,12 @@
+public class Hunnie {
+    public static void main(String[] args) {
+        String lines = "____________________________________________________________";
+        String botName = "Hunnie";
+        System.out.println(lines);
+        System.out.println("Hello! I'm " + botName);
+        System.out.println("What can I do for you?");
+        System.out.println(lines);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(lines);
+    }
+}
