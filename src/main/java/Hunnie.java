090117ee80e877fcd4cad0fc55f2d4c34a56667a
hwangjihun <e0959872@u.nus.edu>
2026-01-22 00:18:43 +0800

    Add feature to echo cmd and exit on bye cmd

--- src/main/java/Hunnie.java
+++ src/main/java/Hunnie.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
+
 public class Hunnie {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String lines = "____________________________________________________________";
         String botName = "Hunnie";
         System.out.println(lines);
         System.out.println("Hello! I'm " + botName);
         System.out.println("What can I do for you?");
         System.out.println(lines);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(lines);
+
+        while (true) {
+            String cmd = scanner.nextLine();
+            System.out.println(lines);
+            if (cmd.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(lines);
+                break;
+            } else {
+                System.out.println(cmd);
+            }
+            System.out.println(lines);
+        }
     }
 }
