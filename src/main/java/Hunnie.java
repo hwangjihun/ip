28d19c9e6fa56b0658ae96c2ab4d2a19bfd600b3
hwangjihun <e0959872@u.nus.edu>
2026-01-22 09:38:21 +0800

    added mark as done / undone feature for the tasks

--- src/main/java/Hunnie.java
+++ src/main/java/Hunnie.java
@@ -6,7 +6,7 @@ public class Hunnie {
 
         String lines = "____________________________________________________________";
         String botName = "Hunnie";
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int numberOfTasks = 0;
 
         System.out.println(lines);
@@ -15,21 +15,36 @@ public class Hunnie {
         System.out.println(lines);
 
         while (true) {
-            String cmd = scanner.nextLine();
+            String command = scanner.nextLine();
+            String[] cmd = command.split(" ", 2);
+
             System.out.println(lines);
-            if (cmd.equals("bye")) {
+            if (cmd[0].equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(lines);
                 break;
             }
-            else if (cmd.equals("list")) {
+            else if (cmd[0].equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < numberOfTasks; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].description);
                 }
             }
+            else if (cmd[0].equals("mark")) {
+                int taskID = Integer.parseInt(cmd[1]) - 1;
+                tasks[taskID].isDone = true;
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" +  tasks[taskID].getStatusIcon() + "] " + tasks[taskID].description);
+            }
+            else if (cmd[0].equals("unmark")) {
+                int taskID = Integer.parseInt(cmd[1]) - 1;
+                tasks[taskID].isDone = false;
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[" +  tasks[taskID].getStatusIcon() + "] " + tasks[taskID].description);
+            }
             else {
-                tasks[numberOfTasks] = cmd;
-                System.out.println("added: "+ cmd);
+                tasks[numberOfTasks] = new Task(command);
+                System.out.println("added: " + command);
                 numberOfTasks++;
             }
             System.out.println(lines);
