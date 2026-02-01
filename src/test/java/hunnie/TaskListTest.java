cd5e9dcf467702aaf659bfeb983cc76e90db9653
hwangjihun <e0959872@u.nus.edu>
2026-02-01 22:21:04 +0800

    Add JUnit Tests for ToDoTest and TaskListTest

---
+++ src/test/java/hunnie/TaskListTest.java
@@ -0,0 +1,84 @@
+package hunnie;
+
+import hunnie.exception.HunnieException;
+import hunnie.task.Task;
+import hunnie.task.TaskList;
+import hunnie.task.ToDo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TaskListTest {
+
+    private TaskList taskList;
+
+    @BeforeEach
+    public void setUp() {
+        taskList = new TaskList();
+    }
+
+    @Test
+    public void size_emptyList_returnsZero() {
+        assertEquals(0, taskList.size());
+    }
+
+    @Test
+    public void add_singleTask_sizeIncreases() {
+        Task task = new ToDo("enjoy life");
+        taskList.add(task);
+        assertEquals(1, taskList.size());
+    }
+
+    @Test
+    public void add_multipleTasks_correctSize() {
+        taskList.add(new ToDo("task 1"));
+        taskList.add(new ToDo("task 2"));
+        taskList.add(new ToDo("task 3"));
+        assertEquals(3, taskList.size());
+    }
+
+    @Test
+    public void get_validIndex_returnsTask() throws HunnieException {
+        Task task = new ToDo("buy Harry Potter book Globet of the Fire");
+        taskList.add(task);
+        Task retrieved = taskList.get(0);
+        assertEquals(task, retrieved);
+    }
+
+    @Test
+    public void get_indexOutOfRange_exceptionThrown() {
+        taskList.add(new ToDo("task"));
+        assertThrows(HunnieException.class, () -> {
+            taskList.get(5);
+        });
+    }
+
+    @Test
+    public void get_emptyList_exceptionThrown() {
+        assertThrows(HunnieException.class, () -> {
+            taskList.get(0);
+        });
+    }
+
+    @Test
+    public void delete_validIndex_taskRemoved() throws HunnieException {
+        taskList.add(new ToDo("task 1"));
+        taskList.add(new ToDo("task 2"));
+
+        taskList.delete(0);
+
+        assertEquals(1, taskList.size());
+        assertEquals("task 2", taskList.get(0).getDescription());
+    }
+
+    @Test
+    public void delete_indexOutOfRange_exceptionThrown() {
+        taskList.add(new ToDo("task"));
+        assertThrows(HunnieException.class, () -> {
+            taskList.delete(5);
+        });
+    }
+
+}
---
+++ src/test/java/hunnie/ToDoTest.java
@@ -0,0 +1,86 @@
+package hunnie;
+
+import hunnie.task.ToDo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ToDoTest {
+
+    @Test
+    public void constructor_validDescription_success() {
+        ToDo todo = new ToDo("read harry potter");
+        assertEquals("read harry potter", todo.getDescription());
+    }
+
+    @Test
+    public void getIsDone_newTask_returnsFalse() {
+        ToDo todo = new ToDo("enjoy life");
+        assertFalse(todo.getIsDone());
+    }
+
+    @Test
+    public void getStatusIcon_notDone_returnsSpace() {
+        ToDo todo = new ToDo("exercise");
+        assertEquals(" ", todo.getStatusIcon());
+    }
+
+    @Test
+    public void getStatusIcon_done_returnsX() {
+        ToDo todo = new ToDo("exercise");
+        todo.mark();
+        assertEquals("X", todo.getStatusIcon());
+    }
+
+    @Test
+    public void mark_task_setsIsDoneTrue() {
+        ToDo todo = new ToDo("finish homework");
+        todo.mark();
+        assertTrue(todo.getIsDone());
+    }
+
+    @Test
+    public void unMark_markedTask_setsIsDoneFalse() {
+        ToDo todo = new ToDo("clean room");
+        todo.mark();
+        todo.unMark();
+        assertFalse(todo.getIsDone());
+    }
+
+    @Test
+    public void toString_notDone_correctFormat() {
+        ToDo todo = new ToDo("esports watch party with friends");
+        assertEquals("[T][ ] esports watch party with friends", todo.toString());
+    }
+
+    @Test
+    public void toString_done_correctFormat() {
+        ToDo todo = new ToDo("read book");
+        todo.mark();
+        assertEquals("[T][X] read book", todo.toString());
+    }
+
+    @Test
+    public void mark_multipleSequence_correctState() {
+        ToDo todo = new ToDo("task");
+
+        assertFalse(todo.getIsDone());
+
+        todo.mark();
+        assertTrue(todo.getIsDone());
+
+        todo.unMark();
+        assertFalse(todo.getIsDone());
+
+        todo.mark();
+        assertTrue(todo.getIsDone());
+    }
+
+    @Test
+    public void toString_anyTask_startsWithT() {
+        ToDo todo = new ToDo("sample task");
+        assertTrue(todo.toString().startsWith("[T]"));
+    }
+}
